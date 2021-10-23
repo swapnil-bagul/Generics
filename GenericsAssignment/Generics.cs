99361d2860a512b5d45bc45372258ff4c0bd2d66
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-23 18:03:02 +0530

    Generics Assignment

---
+++ GenericsAssignment/Generics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsAssignment
+{
+    class Generics
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Assignment");
+        }
+    }
+}
